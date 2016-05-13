c10c6c67f50b72163aa8fc3a8ef57be5c64ed6e3
fengb <contact@fengb.info>
2016-05-13 16:21:42 -0500

    Toggle on and off

--- moar-pop.js
+++ moar-pop.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.tabs.insertCSS({ code: '* { transition: transform 1s ease-in-out !important }' })
-  chrome.tabs.insertCSS({ code: '*:hover { transform: rotate(360deg) !important }' })
+  chrome.tabs.executeScript({ code: 'document.body.classList.toggle("moar-pop")' })
+  chrome.tabs.insertCSS({ file: 'moar-pop.css' })
 })
